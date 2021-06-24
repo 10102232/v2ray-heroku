1f63a2f812a1fb54ddfbb6c3a2c1be2387c4b1e7
10102232 <31153532+10102232@users.noreply.github.com>
2021-06-24 12:46:04 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -4,13 +4,10 @@
 mkdir /tmp/v2ray
 mkdir /tmp/123
 wget -q https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip -O /tmp/v2ray/v2ray.zip
-wget -q https://codeload.github.com/10102232/n2n/zip/refs/tags/123 -O /tmp/v2ray/123.zip
 unzip /tmp/v2ray/v2ray.zip -d /tmp/v2ray
-unzip /tmp/v2ray/123.zip -d /tmp/123
 install -m 755 /tmp/v2ray/v2ray /usr/local/bin/v2ray
 install -m 755 /tmp/v2ray/v2ctl /usr/local/bin/v2ctl
 install -m 755 /tmp/123/v2ctl /usr/local/bin/v2ctl
-install -m 755 /tmp/123/n2n-123/1 /usr/local/bin/1
 # Remove temporary directory
 rm -rf /tmp/v2ray
 
@@ -46,4 +43,3 @@ EOF
 
 # Run V2Ray
 /usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.json
-/usr/local/bin/1 -a 10.255.255.1 -c fanqiang -k fanqiang -r -b -l 132.232.37.251:10000 -f &
